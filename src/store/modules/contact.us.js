e5c8d7c3f2183a1d768e0a73636748fb34cbe422
addempsea <babatundeademola4@gmail.com>
2020-05-30 22:07:26 +0100

    feature(vuex): add set up for vuex

---
+++ src/store/modules/contact.us.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios';
+
+const state = {};
+const getters = {};
+const actions = {};
+const mutations = {};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+};
